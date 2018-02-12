73137f05b79a2a4808fd94125b09d1b33cb344df
revoljucioner <moderya7@gmail.com>
2018-02-12 12:31:25 +0200

    Just test modificied Post method

---
+++ Speed_Server/Controllers/SpeedServerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpeedServerApi.Models;
+
+namespace SpeedServerApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SpeedServer")]
+    public class SpeedServerController : Controller
+    {
+        private readonly SpeedServerContext _context;
+
+        public SpeedServerController(SpeedServerContext context)
+        {
+            _context = context;
+
+            if (_context.SpeedModels.Count() == 0)
+            {
+                _context.SpeedModels.Add(new SpeedModel { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] SpeedModel item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.SpeedModels.Add(item);
+            _context.SaveChanges();
+
+            //return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+            //
+            item.latitude = 5;
+            return new ObjectResult(item);
+        }
+    }
+}
---
+++ Speed_Server/Models/SpeedModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SpeedServerApi.Models
+{
+    public class SpeedModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public int latitude { get; set; }
+    }
+}
