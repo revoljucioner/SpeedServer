bf1fa314c60013ed93193a1adec4d2f7dbfe6849
revoljucioner <moderya7@gmail.com>
2018-02-20 10:52:23 +0200

    Исправил код в RemoveExtraPoints. Добавил полную проверку находятся ли 3 точки на прямой и образуют ли два промежутка с одинаковой скоростью. Необходимо написать сам алгоритм удаления лишних точек.

--- Speed_Server/Models/SpeedModel.cs
+++ Speed_Server/Models/SpeedModel.cs
@@ -6,6 +6,7 @@ namespace SpeedServerApi.Models
 {
     public class SpeedModel
     {
+        private double EarthRadius = 6371.009;
         public SnappedPoint[] snappedPoints { get; set; }
 
         public SpeedModel()
@@ -24,20 +25,19 @@ namespace SpeedServerApi.Models
             this.snappedPoints = snappedPointsList.ToArray();
         }
 
-        public void RemoveExtraPoints(double accuracyLocation, double accuracyTime)
+        public void RemoveExtraPoints(double accuracyLocation, double accuracySpeed)
         {
             // TODO
             // или переделать поле класса на лист, чтобі можно біло єлемент ремувать или тут создавать новый массив, заполнять его не лишними точками и переприсваивать его на поле экземпляра класса 
+            //
+            // возможно использовать рекурсию
             var snappedPointsLength = snappedPoints.Length;
             if (snappedPointsLength > 3)
             {
                 for (var i = 1; i < snappedPointsLength - 1; i++)
                 {
-                    // TODO
-                    // добавить метод получения сущности локации по индексу, а то это капец каждый раз писать столько через точку 
-                    bool pointsBelongToSameLine = PointsBelongToSameLine(LocationByIndex(i - 1), LocationByIndex(i), LocationByIndex(i + 1), accuracyLocation);
-                    bool pointsMakeSameTimeSpan = PointsMakeSameTimeSpan(TimeByIndex(i - 1), TimeByIndex(i), TimeByIndex(i + 1), accuracyTime);
-                    if (pointsBelongToSameLine & pointsMakeSameTimeSpan)
+                    bool pointsMakeSameSpeedInSameLine = PointsMakeSameSpeedInSameLine(LocationByIndex(i - 1), LocationByIndex(i), LocationByIndex(i + 1), accuracyLocation, accuracySpeed);
+                    if (pointsMakeSameSpeedInSameLine)
                     {
                         // TODO
                     }
@@ -45,14 +45,12 @@ namespace SpeedServerApi.Models
             }
         }
 
-        // TODO
-        // проверить учет аккуратности
         private bool PointsBelongToSameLine(Location Point1, Location Point2, Location Point3, double accuracyLocation)
         {
-            var sameLatitude = (Point3.latitude - Point1.latitude)/(Point2.latitude - Point1.latitude);
-            var sameLongitude = (Point3.longitude - Point1.longitude)/(Point2.longitude - Point1.longitude);
+            var sameLatitude = (Point3.latitude - Point1.latitude) / (Point2.latitude - Point1.latitude);
+            var sameLongitude = (Point3.longitude - Point1.longitude) / (Point2.longitude - Point1.longitude);
 
-            if ((Math.Abs(sameLatitude-sameLongitude))<= accuracyLocation)
+            if ((Math.Abs(sameLatitude - sameLongitude)) <= accuracyLocation)
             {
                 return true;
             }
@@ -62,16 +60,19 @@ namespace SpeedServerApi.Models
             }
         }
 
-        // TODO
-        // проверить учет аккуратности
-        // тут должна быть поправка не на время, а на скорость
-        private bool PointsMakeSameTimeSpan(DateTime time1, DateTime time2, DateTime time3, double accuracyTime)
+        private bool PointsMakeSameSpeed(LocationTime Point1, LocationTime Point2, LocationTime Point3, double accuracySpeed)
         {
-            var diffBetween1and2PointTime = time2 - time1;
-            var diffBetween2and3PointTime = time3 - time2;
+            // kilometers
+            var Point1Point2Distance = DistanceBetweenPoints(Point1, Point2) * EarthRadius;
+            var Point2Point3Distance = DistanceBetweenPoints(Point2, Point3) * EarthRadius;
+            // hours
+            var Point1Point2Time = (Point2.time - Point1.time).TotalHours;
+            var Point2Point3Time = (Point3.time - Point2.time).TotalHours;
+            // km/h
+            var Point1Point2Speed = Point1Point2Distance / Point1Point2Time;
+            var Point2Point3Speed = Point2Point3Distance / Point2Point3Time;
 
-            if ((diffBetween2and3PointTime - diffBetween1and2PointTime).TotalMilliseconds <= accuracyTime)
-
+            if ((Math.Abs(Point1Point2Speed - Point2Point3Speed)) <= accuracySpeed)
             {
                 return true;
             }
@@ -81,14 +82,29 @@ namespace SpeedServerApi.Models
             }
         }
 
-        private Location LocationByIndex(int i)
+        private double DistanceBetweenPoints(Location Point1, Location Point2)
+        {
+            var Point1Point2Distance = Math.Acos(Math.Sin(Point1.latitude) * Math.Sin(Point2.latitude) + Math.Cos(Point1.latitude) * Math.Cos(Point2.latitude) * Math.Cos(Point1.longitude - Point2.longitude));
+            return Point1Point2Distance;
+        }
+
+        private bool PointsMakeSameSpeedInSameLine(LocationTime Point1, LocationTime Point2, LocationTime Point3, double accuracyLocation, double accuracySpeed)
+        {
+            bool pointsBelongToSameLine = PointsBelongToSameLine(Point1, Point2, Point3, accuracyLocation);
+            bool pointsMakeSameSpeed = PointsMakeSameSpeed(Point1, Point2, Point3, accuracySpeed);
+            if (pointsBelongToSameLine & pointsMakeSameSpeed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private LocationTime LocationByIndex(int i)
         {
             return snappedPoints[i].Location;
-        }
-
-        private DateTime TimeByIndex(int i)
-        {
-            return snappedPoints[i].Location.time;
         }
     }
 }
