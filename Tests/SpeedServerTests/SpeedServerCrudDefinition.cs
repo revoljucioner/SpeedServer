a57d8b48d0cf11468c8c30f160b165a912882252
revoljucioner <moderya7@gmail.com>
2019-01-02 16:50:24 +0200

    Add assertion to test

--- Tests/SpeedServerTests/SpeedServerCrudDefinition.cs
+++ Tests/SpeedServerTests/SpeedServerCrudDefinition.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Configuration;
-using System.IO;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using FluentAssertions;
 using NUnit.Framework;
-using Tests.Environment;
-using Tests.Helpers;
 using Tests.Steps;
 
 namespace Tests.SpeedServerTests
@@ -23,6 +18,8 @@ namespace Tests.SpeedServerTests
         {
             var speedServerSteps = new SpeedServerSteps();
             var httpResponseMessage = await speedServerSteps.PostSpeedServerApiGetResponse("[\r\n  {\r\n    \"location\": {\r\n      \"latitude\": -35.2807341,\r\n      \"longitude\": 149.1291511\r\n    },\r\n    \"time\": \"2018-02-18T01:00:00.0000000+00:00\"\r\n  },\r\n  {\r\n    \"location\": {\r\n      \"latitude\": -35.2807342,\r\n      \"longitude\": 149.1291512\r\n    },\r\n    \"time\": \"2018-02-18T01:01:00.0000000+00:00\"\r\n  }]");
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
         }
     }
 }
